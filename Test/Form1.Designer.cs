5eed290001fd2e8c415ee495247b29bb853198fb
Solodyagin <solodyagin@gmail.com>
2018-09-14 15:38:28 +0300

    Added Test application

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(423, 10);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(95, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Просклонять";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 14;
+			this.listBox1.Location = new System.Drawing.Point(12, 38);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(506, 368);
+			this.listBox1.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Петров Василий Иванович",
+            "Коновалова Ольга Михайловна",
+            "Шелест Василиса Витальевна",
+            "Трубопрокатный цех"});
+			this.comboBox1.Location = new System.Drawing.Point(12, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(405, 21);
+			this.comboBox1.TabIndex = 3;
+			this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(530, 434);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Тест NameCaseLib";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Test
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrWhiteSpace(comboBox1.Text))
+			{
+				listBox1.Items.Clear();
+
+				NameCaseLib.Ru ru = new NameCaseLib.Ru();
+				listBox1.Items.Add("Именительный  (кто? что?): " + ru.Q(comboBox1.Text, NameCaseLib.NCL.Padeg.IMENITLN));
+				listBox1.Items.Add("Родительный (кого? чего?): " + ru.Q(comboBox1.Text, NameCaseLib.NCL.Padeg.RODITLN));
+				listBox1.Items.Add("Дательный   (кому? чему?): " + ru.Q(comboBox1.Text, NameCaseLib.NCL.Padeg.DATELN));
+				listBox1.Items.Add("Винительный  (кого? что?): " + ru.Q(comboBox1.Text, NameCaseLib.NCL.Padeg.VINITELN));
+				listBox1.Items.Add("Творительный  (кем? чем?): " + ru.Q(comboBox1.Text, NameCaseLib.NCL.Padeg.TVORITELN));
+				listBox1.Items.Add("Предложны (о ком? о чём?): " + ru.Q(comboBox1.Text, NameCaseLib.NCL.Padeg.PREDLOGN));
+			}
+		}
+
+		private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				button1.PerformClick();
+			}
+		}
+	}
+}
